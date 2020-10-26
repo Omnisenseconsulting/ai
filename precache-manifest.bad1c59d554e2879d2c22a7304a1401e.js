a2ea5fc2581d08c35261e19a77e3a97d9e077421
Omnisenseconsulting <omnisenseconsulting@gmail.com>
2020-10-26 23:36:46 +0530

    Updates

---
+++ precache-manifest.bad1c59d554e2879d2c22a7304a1401e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e6ed881c47880f446f35d8c416be13",
+    "url": "/ai/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/ai/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "79807158260d82fca83a",
+    "url": "/ai/static/css/main.8b58b3cd.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/ai/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ai/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79807158260d82fca83a",
+    "url": "/ai/static/js/main.e80254e6.chunk.js"
+  },
+  {
+    "revision": "3b80c77aa97214cec495",
+    "url": "/ai/static/js/runtime-main.127db645.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/ai/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/ai/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/ai/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/ai/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/ai/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/ai/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/ai/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/ai/static/media/software.43049cae.jpeg"
+  }
+]);
